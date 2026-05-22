7b44783ee42539b9a025d99510ec5bba1ddc64ce
deepakactowiz <deepakk.actowiz@gmail.com>
2026-05-22 20:00:36 +0530

    implemented duplicate logic

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def get_details():
 @app.post("/user")
 def create_user(user:User):
     user_details.append(user)
+    for each_user in user_details:
+        if each_user.id == user.id:
+            return "id is already exists..."
     return{
         "message":"Data inseted successfully...",
         "result":user_details
