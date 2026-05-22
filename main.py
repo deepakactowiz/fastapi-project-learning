7bad4a092f4d1e020cef6dc4cfab67d8aa817391
deepakactowiz <deepakk.actowiz@gmail.com>
2026-05-22 19:48:20 +0530

    implemented post method

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app= FastAPI()
 
+class User(BaseModel):
+    id:int
+    name:str
+    city:str
+    status:bool
+
+user_details=[]
 @app.get("/")
 def get_details():
     return{
         "message":"Welcom to the New World!"
     }
+@app.post("/user")
+def create_user(user:User):
+    user_details.append(user)
+    return{
+        "message":"Data inseted successfully...",
+        "result":user_details
+    }
+
+
